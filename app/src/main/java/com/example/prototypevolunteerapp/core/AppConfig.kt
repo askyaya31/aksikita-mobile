23d85def6a5713866de199102393cba1ede84abd
askyaya31 <saskyaaliyaazizah06@gmail.com>
2026-06-07 19:08:23 +0700

    Update production URL and set BASE_URL to production

--- app/src/main/java/com/example/prototypevolunteerapp/core/AppConfig.kt
+++ app/src/main/java/com/example/prototypevolunteerapp/core/AppConfig.kt
@@ -1,9 +1,17 @@
 package com.example.prototypevolunteerapp.core
 
+// object AppConfig {
+//     private const val URL_EMULATOR   = "http://10.0.2.2:8000/"
+//     private const val URL_DEVICE     = "http://192.168.1.100:8000/"
+//     private const val URL_PRODUCTION = "https://aksikita.example.com/"
+
+//     const val BASE_URL = URL_EMULATOR
+// }
+
 object AppConfig {
     private const val URL_EMULATOR   = "http://10.0.2.2:8000/"
     private const val URL_DEVICE     = "http://192.168.1.100:8000/"
-    private const val URL_PRODUCTION = "https://aksikita.example.com/"
+    private const val URL_PRODUCTION = "https://pantyhose-semantic-virtuous.ngrok-free.dev"
 
-    const val BASE_URL = URL_EMULATOR
+    const val BASE_URL = URL_PRODUCTION
 }
