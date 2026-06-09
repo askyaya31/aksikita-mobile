0d5b1ede16f58bc38b34ba16d3980f3fd9c66a85
askyaya31 <saskyaaliyaazizah06@gmail.com>
2026-06-10 01:40:00 +0700

    Add ngrok interceptor to OkHttpClient

--- app/src/main/java/com/example/prototypevolunteerapp/di/NetworkModule.kt
+++ app/src/main/java/com/example/prototypevolunteerapp/di/NetworkModule.kt
@@ -48,9 +48,17 @@ object NetworkModule {
                 .body(fixedBody.toResponseBody(contentType))
                 .build()
         }
+        
+        val ngrokInterceptor = Interceptor { chain ->
+            val request = chain.request().newBuilder()
+                .addHeader("ngrok-skip-browser-warning", "true")
+                .build()
+            chain.proceed(request)
+        }
 
         return OkHttpClient.Builder()
             .addInterceptor(authInterceptor)
+            .addInterceptor(ngrokInterceptor)
             .addInterceptor(localhostFixInterceptor)
             .addInterceptor(loggingInterceptor)
             .build()
